f01dbccb71f2cbdd4f298abddd6617267b25514f
oskaryil <15bayi@stockholmscience.se>
2017-01-18 10:16:06 +0100

    auth

---
+++ config/auth.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'facebookAuth': {
+    'clientID': 'CLIENT_ID',
+    'clientSecret': 'CLIENT_SECRET',
+    'callbackURL': 'CALLBACK_URL'
+  },
+  'googleAuth': {
+    'clientID': 'CLIENT_ID',
+    'clientSecret': 'CLIENT_SECRET',
+    'callbackURL': 'CALLBACK_URL'
+  },
+  'githubAuth': {
+    'clientID': 'CLIENT_ID',
+    'clientSecret': 'CLIENT_SECRET',
+    'callbackURL': 'CALLBACK_URL'
+  }
+}
