485a0a5e74d915a69d664a48b1bc485507c208a0
Anton Myrberg <antonmyrberg@localhost.localdomain>
2017-01-30 18:57:32 +0100

    added some jquery to checkboxes on the add form

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
         // there is an image in this div, do something...
         $(this).closest('.cards-header').addClass('p-text-shadow');
     }
+
 });
 
+$('#toilet-check').click(function() {
+    if( $(this).prop('checked')) {
+        $(".toilet-code").css('visibility', 'visible');
+    }
+    else  {
+      $(".toilet-code").css('visibility', 'hidden');
+    }
+});
 });
